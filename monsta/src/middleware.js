0e601e3ba4b5abf2b390ff82395c21aeae35a5a7
hemanth-k-malviya <hemanthkmalviya@gmail.com>
2026-03-16 13:06:27 +0530

    middleware

--- monsta/src/middleware.js
+++ monsta/src/middleware.js
@@ -1,16 +1,18 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-    //redirect if user is not  logged in
+
     const isLoggedIn = request.cookies.get('user_login')?.value;
 
-    if ((!isLoggedIn) && request.nextUrl.pathname.startsWith('/my-dashboard')) {
-        return NextResponse.redirect(new URL('/login-register', request.url))
+    // redirect if user not logged in
+    if (!isLoggedIn && request.nextUrl.pathname.startsWith('/my-dashboard')) {
+        return NextResponse.redirect(new URL('/login-register', request.url));
     }
-    if ((isLoggedIn) && request.nextUrl.pathname.startsWith('/login-register')) {
-        return NextResponse.redirect(new URL('/my-dashboard', request.url))
+
+    // redirect if user already logged in
+    if (isLoggedIn && request.nextUrl.pathname.startsWith('/login-register')) {
+        return NextResponse.redirect(new URL('/my-dashboard', request.url));
     }
+
+    return NextResponse.next();
 }
-    // Handle simple requests
-    const response = NextResponse.next()
-    return response;
